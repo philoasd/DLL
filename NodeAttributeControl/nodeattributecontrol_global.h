9b43c97c34f989e40b88566988938fde019e059a
phil <1742856967qq.com@gmail.com>
2023-11-24 17:55:26 +0800

    Add files via upload

---
+++ NodeAttributeControl/nodeattributecontrol_global.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <QtCore/qglobal.h>
+
+#ifndef BUILD_STATIC
+# if defined(NODEATTRIBUTECONTROL_LIB)
+#  define NODEATTRIBUTECONTROLSHARED_EXPORT Q_DECL_EXPORT
+# else
+#  define NODEATTRIBUTECONTROLSHARED_EXPORT Q_DECL_IMPORT
+# endif
+#else
+# define NODEATTRIBUTECONTROLSHARED_EXPORT
+#endif
