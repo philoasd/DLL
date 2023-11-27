9117d89f36c3f01dc583c9fdceba633646aa8a34
phil <1742856967qq.com@gmail.com>
2023-11-27 15:50:31 +0800

    Add files via upload

---
+++ ImageProcessingLibrary/ImageProcessingLibrary/OpenCVLibrary.cpp
@@ -0,0 +1,73 @@
+﻿#include "pch.h"
+#include "OpenCVLibrary.h"
+
+cv::Mat OpenCVLibrary::Threshold(cv::Mat inputImage, int leftThresholdValue, int rightThresholdValue, cv::Rect rect)
+{
+	// 提取ROI区域
+	cv::Mat roiImage;
+	if (!CheckROI(inputImage, roiImage, rect)) { return inputImage; }
+
+	// 二值化
+	cv::Mat binaryRoi;
+	cv::threshold(roiImage, binaryRoi, leftThresholdValue, rightThresholdValue, cv::THRESH_BINARY);
+
+	if (inputImage(rect).channels() == 3) {
+		cv::cvtColor(binaryRoi, binaryRoi, cv::COLOR_GRAY2BGR);
+	}
+
+	if (rect.empty()) {
+		return binaryRoi;
+	}
+
+	// 将二值化后的图像拷贝到原图中
+	binaryRoi.copyTo(inputImage(rect));
+
+	return inputImage;
+}
+
+cv::Mat OpenCVLibrary::AutoThreshold(cv::Mat inputImage, cv::Rect rect)
+{
+	// 提取ROI区域
+	cv::Mat roiImage;
+	if (!CheckROI(inputImage, roiImage, rect)) { return inputImage; }
+
+	// 自适应阈值处理
+	cv::Mat adaptiveThresholdImage;
+	adaptiveThreshold(roiImage, adaptiveThresholdImage, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 2);
+
+	if (inputImage(rect).channels() == 3) {
+		cv::cvtColor(adaptiveThresholdImage, adaptiveThresholdImage, cv::COLOR_GRAY2BGR);
+	}
+
+	if (rect.empty()) {
+		return adaptiveThresholdImage;
+	}
+
+	// 将二值化后的图像拷贝到原图中
+	adaptiveThresholdImage.copyTo(inputImage(rect));
+
+	return inputImage;
+}
+
+bool OpenCVLibrary::CheckROI(cv::Mat& inputImage, cv::Mat& roiImage, cv::Rect& rect)
+{
+	if (inputImage.empty())
+	{
+		return false;
+	}
+
+	// 提取ROI区域
+	if (rect.empty()) {
+		roiImage = inputImage;
+	}
+	else {
+		roiImage = inputImage(rect);
+	}
+
+	// 转换为灰度图
+	if (roiImage.channels() == 3) {
+		cv::cvtColor(roiImage, roiImage, cv::COLOR_BGR2GRAY);
+	}
+
+	return true;
+}
---
+++ ImageProcessingLibrary/ImageProcessingLibrary/OpenCVLibrary.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <opencv2/opencv.hpp>
+
+class _declspec(dllexport) OpenCVLibrary
+{
+public:
+	/// <summary>
+	/// 二值化图像
+	/// </summary>
+	/// <param name="input">输入图像</param>
+	/// <param name="leftThresholdValue">左阈值</param>
+	/// <param name="rightThresholdValue">右阈值</param>
+	/// <param name="rect">ROI区域,默认为空</param>
+	cv::Mat Threshold(cv::Mat inputImage, int leftThresholdValue, int rightThresholdValue, cv::Rect rect = cv::Rect());
+
+	/// <summary>
+	/// 自动二值化图像
+	/// </summary>
+	/// <param name="inputImage">输入图像</param>
+	/// <param name="rect">ROI区域,默认为空</param>
+	/// <returns></returns>
+	cv::Mat AutoThreshold(cv::Mat inputImage, cv::Rect rect = cv::Rect());
+
+private:
+	// 检查是否有ROI区域
+	bool CheckROI(cv::Mat& inputImage, cv::Mat& roiImage, cv::Rect& rect);
+};
+
